5df9ddebf40dc4b1ea6d5d09b34f10a8ac692f08
Jzsky <jinkang396@hotmail.com>
2023-01-31 19:15:12 -0500

    update replicate.py

--- replicate.py
+++ replicate.py
@@ -3,10 +3,19 @@ import os
 
 class replicate:
 
-    def __init__(self,file):
-        self.file = file
+    def __init__(self,data=None):
+        self.file = data
 
 
     def getfile(self):
         return self.file
-    
+
+    def replicate_file(self, path="%temp%", ostype="windows"):
+        shutil.copy(__file__, 'temp/copied_script_name.py') 
+
+    def self_replicate(self):
+        pass
+
+
+r = replicate()
+r.self_replicate()
